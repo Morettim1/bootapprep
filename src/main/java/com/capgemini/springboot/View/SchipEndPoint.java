c61f4562e928ac3e89086e7dfe4654c9217634a2
Morettim1 <maurice.moretti@capgemini.com>
2019-02-13 10:58:25 +0100

    Derde commit. Nu met SchipEndPoint die iets direct print op de pagina "hallo"

---
+++ src/main/java/com/capgemini/springboot/View/SchipEndPoint.java
@@ -0,0 +1,13 @@
+package com.capgemini.springboot.View;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SchipEndPoint {
+    @GetMapping("hallo")
+    public String lopen(){
+        System.out.println("go");
+        return "jojo";
+    }
+}
